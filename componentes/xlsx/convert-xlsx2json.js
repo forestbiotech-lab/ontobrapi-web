c535492507bf5af4af2e1c8a15e504346a4cea9b
Bruno Costa <brunovasquescosta@gmail.com>
2021-10-14 13:38:42 +0100

    Changes the exported data. Now it exports the jsheet data without any tweak

--- componentes/xlsx/convert-xlsx2json.js
+++ componentes/xlsx/convert-xlsx2json.js
@@ -9,12 +9,6 @@ module.exports=function(file){
   let xlsxPath=`${file.filePath}/${file.name}`
   var xlsx = XLSX.readFile(xlsxPath);
 
-
-  let jSheet={
-    worksheets:xlsx.SheetNames,
-    sheets:xlsx.Sheets
-  }
-
-  return jSheet
+  return xlsx
 }
 
