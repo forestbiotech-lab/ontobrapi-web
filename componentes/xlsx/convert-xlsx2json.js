eff35650c0d52db6e4accb25aac3acb1589984b4
Bruno Costa <brunovasquescosta@gmail.com>
2021-11-11 17:49:58 +0000

    Add new features: 1) csv parsed data and 2) headers properly calculated from csv. Getting data from json parsed data might omit columns if cell is blank. Also fixes missing object in Workbook.Sheets for ODS compatability

--- componentes/xlsx/convert-xlsx2json.js
+++ componentes/xlsx/convert-xlsx2json.js
@@ -10,10 +10,46 @@ module.exports=function(file){
   let xlsxPath=`${file.filePath}/${file.name}`
   var xlsx = XLSX.readFile(xlsxPath);
   xlsx.jsonSheets={}
-  
+  xlsx.csvSheets={}
+  xlsx.headers={}
+  makeCompatible(xlsx)
   for (sheetName of xlsx.SheetNames){
     xlsx.jsonSheets[sheetName]=XLSX.utils.sheet_to_json(xlsx.Sheets[sheetName])
+    xlsx.csvSheets[sheetName]=XLSX.utils.sheet_to_csv(xlsx.Sheets[sheetName])
+    xlsx.headers[sheetName]=getHeaders(xlsx.csvSheets[sheetName])
+    
   }
   return xlsx
 }
 
+function getHeaders(csvSheets){
+  let emptyNumber=""
+  let headerLine=0
+  let headers=csvSheets.split("\n")[headerLine].split(",")
+  headers.forEach((column,index)=>{
+    if(column==""){
+      column="__EMPTY"+emptyNumber 
+      if(emptyNumber==""){
+        emptyNumber="_1"
+      }else{
+        emptyNumber="_"+(parseInt(emptyNumber.replace(/_+/g,""))+1)
+      }
+    }
+    headers[index]=column
+  })
+  return headers
+}
+
+function makeCompatible(jSheet){ 
+  if(!jSheet.Workbook.Sheets) jSheet.Workbook.Sheets={}
+  jSheet.SheetNames.forEach((sheetName,index)=>{
+    if(!jSheet.Workbook.Sheets[sheetName]){
+      jSheet.Workbook.Sheets[sheetName]={
+        id:`sheet${index}`,
+        name:sheetName,
+        SheetId:index,
+        Sheetid:index,
+      }
+    }
+  })
+}
