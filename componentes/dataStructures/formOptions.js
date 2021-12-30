971ec0931be8b0ae9b88df34e2e923a1c4dd23fc
Bruno Costa <brunovasquescosta@gmail.com>
2021-12-30 19:31:12 +0000

    Adds the folder to start placing the datastructures used by the components

---
+++ componentes/dataStructures/formOptions.js
@@ -0,0 +1,30 @@
+module.exports={
+    type: [
+        {
+            label: "Class",
+            name: "class"
+        }, {
+            label: "Data Property",
+            name: "dataProperty",
+        }, {
+            label: "Not Applicable",
+            name: "NA"
+        }],
+    name: {},
+    valueType: [{
+        label: "Named Node",
+        name: "named_node"
+    }, {
+        label: "String (xsd:string)",
+        name: "xsd:string"
+    }, {
+        label: "Integer (xsd:integer)",
+        name: "integer"
+    }, {
+        label: "Not Applicable",
+        name: "NA"
+    }],
+    "naming_scheme": {
+        displayName: "Naming Scheme"
+    }
+}
