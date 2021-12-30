ff9ebfa310cb8d8dc0b8617f8a0619f0897aa739
Bruno Costa <brunovasquescosta@gmail.com>
2021-12-30 15:14:09 +0000

    Fixes upload another file. Confirms you want to continue and reloads page

--- public/javascripts/loaders.js
+++ public/javascripts/loaders.js
@@ -84,7 +84,13 @@ $('document').ready(function(){
         self.closest('div').nextAll('.preview-table').removeClass('d-none')
 
         function resetUploadAbility(){
-          $(".upload-augment-file").text("Upload another file")  
+          $(".upload-augment-file").text("Upload another file")
+          $(".upload-augment-file").unbind('click')
+          $(".upload-augment-file").click(function(){
+            if(confirm("All unsaved mappings will be lost! Proceed?")){
+             window.location.reload();
+            }
+          })
         }
         resetUploadAbility()  
         ntGenerationButton()
