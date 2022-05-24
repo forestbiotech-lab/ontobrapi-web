e891e1a545fc67f84a04ffb0ecf94a9436860828
Bruno Costa <brunovasquescosta@gmail.com>
2022-05-24 15:17:18 +0100

    Adds inferred Object and Data properties based on the originating class

---
+++ componentes/sparql/inferredRelationships.js
@@ -0,0 +1,134 @@
+const SparqlClient = require('sparql-http-client')
+
+const sparql=require('./../../.config').sparql
+
+let host=sparql.host
+let port=sparql.port
+
+const endpointUrl = `http://${host}:${port}/sparql`
+
+let subject = 's'
+let object = 'o'
+let predicate = 'p'
+
+//Possible solution
+// TODO REMOVE?
+Array.prototype.forEachAsync = async function (fn) {
+    for (let t of this) { await fn(t) }
+}
+//TODO REMOVE?
+Array.prototype.forEachAsyncParallel = async function (fn) {
+    await Promise.all(this.map(fn));
+}
+
+
+function objectProperties(className) {
+
+    let query=`
+        PREFIX miappe:  <http://purl.org/ppeo/PPEO.owl#>
+        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl:     <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT DISTINCT ?ObjectProperty ?destination
+        FROM <http://purl.org/ppeo/PPEO.owl#> 
+        WHERE {
+            VALUES ?class { miappe:${className} } .
+        {
+            ?ObjectProperty            rdf:type            owl:ObjectProperty         .
+            ?ObjectProperty    rdfs:domain                 ?class                     .
+            ?ObjectProperty    rdfs:range                  ?destination               .
+            ?class             rdf:type                    owl:Class                  .
+        } UNION {
+            ?destination               ?ObjectProperty            ?class                     .
+            ?destination               rdfs:subClassOf            ?class                     .
+        } UNION {
+            ?class             rdfs:subClassOf            ?node                       .
+            ?node               rdf:type                   owl:Restriction            .
+            ?node               owl:onProperty             ?ObjectProperty            .
+            ?node               owl:someValuesFrom         ?destination               .
+        }}`
+    //TODO Check if this has the right origin ou destination
+    return sparqlQuery(query)
+
+}
+
+
+
+function dataProperties(className,objectProperty) {
+
+    let query=`
+         PREFIX miappe:  <http://purl.org/ppeo/PPEO.owl#>
+         PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+         PREFIX owl:     <http://www.w3.org/2002/07/owl#>
+         PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+         PREFIX schema:  <http://www.w3.org/2001/XMLSchema#>
+
+         SELECT DISTINCT ?class ?dataProperty  ?range
+         FROM <http://purl.org/ppeo/PPEO.owl#> 
+         WHERE {
+            VALUES ?class { miappe:${className} } .
+            {
+               ?dataProperty     rdf:type          owl:DatatypeProperty   . 
+               ?dataProperty     rdfs:domain       ?class                 .
+               ?dataProperty     rdfs:range        ?range                 .
+            } UNION {
+               ?class            rdfs:subClassOf   ?node                  .
+               ?node             owl:onProperty    ?dataProperty          .
+               ?node             owl:onDataRange   ?range                 .
+
+            } UNION {
+               ?class            rdfs:subClassOf    ?node                  .
+               ?node             owl:onProperty     ?dataProperty          .
+               ?node             owl:onDataRange    ?node2                 .
+               ?node2            owl:unionOf        ?node3                 .
+               ?node3            ?rangeType        ?range                  .
+            }UNION {
+               ?class            rdfs:subClassOf    ?node                  .
+               ?node             owl:onProperty     ?dataProperty          .
+               ?node             owl:onDataRange    ?node2                 .
+               ?node2            owl:unionOf        ?node3                 .
+               ?node3            ?rangeType1        ?range4                .
+               ?range4           ?rangeType2        ?range                 .
+            }
+            FILTER ( !isBlank(?range) && ?range != rdf:nil ) 
+         }`
+    //TODO Check if this has the right origin ou destination
+    return sparqlQuery(query)
+
+}
+
+function sparqlQuery(query){
+    return new Promise((res,rej)=>{
+        const client = new SparqlClient({ endpointUrl })
+        client.query.select(query).then(stream=>{
+            let result=[]
+            stream.on('data', row => {
+                let resultTriple={}
+                Object.entries(row).forEach(([key, value]) => {
+                    resultTriple[key]=value.value
+                })
+                result.push(resultTriple)
+            })
+            stream.on('error', err => {
+                rej(err)
+            })
+            stream.on('end', err=>{
+                res(result)
+            })
+        }).catch(err=>{
+            let message=err.msg
+            let stack=err.stack
+            rej({err,message,stack})
+        })
+    })
+}
+
+
+
+module.exports = {
+    objectProperties,
+    dataProperties
+
+};
+
--- routes/query.js
+++ routes/query.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 let xsd = require("@ontologies/xsd")
 let classProperties = require('./../componentes/sparql/classProperties')
+let inferredRelationship = require('./../componentes/sparql/inferredRelationships')
 let formOptions=require('./../componentes/dataStructures/formOptions')
 let info=require('./../componentes/dataStructures/info')
 
@@ -29,5 +30,27 @@ router.get('/dataStructures/formOptions/', function(req, res) {
   res.json(formOptions)
 });
 
+router.get('/inferred/objectProperty/:class',(req,res)=>{
+  let className=req.params.class
+  inferredRelationship.objectProperties(className).then(result=>{
+      res.json(result)
+  }).catch(err=>{
+    let message=err.msg
+    let stack=err.stack
+    res.json({err,message,stack})
+  })
+
+})
+router.get('/inferred/dataProperty/:class',(req,res)=>{
+  let className=req.params.class
+  inferredRelationship.dataProperties(className).then(result=>{
+    res.json(result)
+  }).catch(err=>{
+    let message=err.msg
+    let stack=err.stack
+    res.json({err,message,stack})
+  })
+
+})
 
 module.exports = router;
