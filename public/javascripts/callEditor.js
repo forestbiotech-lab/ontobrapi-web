c9da01e1cfc3cc9d6e5693a9714f523600a2b97a
Bruno Costa <netbofia@hotmail.com>
2022-06-01 22:16:15 +0100

    Retry on fail to load.

--- public/javascripts/callEditor.js
+++ public/javascripts/callEditor.js
@@ -1,5 +1,14 @@
-let scriptTarget=document.getElementById('actions')
-let url="/javascripts/callEditor/init.js";
-let script = document.createElement('script');
-script.src = url;
-scriptTarget.parentNode.insertBefore(script, scriptTarget);
+try {
+    loadAsyncScript()
+}catch (e){
+    setTimeout(()=>loadAsyncScript(),2000)
+}
+
+
+function loadAsyncScript(){
+    let scriptTarget = document.getElementById('actions')
+    let url = "/javascripts/callEditor/init.js";
+    let script = document.createElement('script');
+    script.src = url;
+    scriptTarget.parentNode.insertBefore(script, scriptTarget);
+}
